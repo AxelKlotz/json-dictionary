bb6d11078f749e7abe428d128f65a6946fe015ae
AxelRK32 <114733972+AxelRK32@users.noreply.github.com>
2024-01-16 15:51:20 +0100

    Add Writing Json file

--- Manager.cs
+++ Manager.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace JsonDictionary
 {
@@ -10,15 +11,15 @@ namespace JsonDictionary
         //Dictionary till Json
         public string CreateJsonFromDict(Dictionary<string, object> dict)
         {
-            if (!dict.Any())
-            {
+            if (dict == null)
+                throw new ArgumentNullException();
+            else if (!dict.Any())
                 throw new ArgumentException();
-            }
-            else
-            {
-                string json = JsonConvert.SerializeObject(dict);
-                return json;
-            }
+            else if (dict.ContainsValue(null))
+                throw new NullReferenceException();
+
+            string json = JsonConvert.SerializeObject(dict);
+            return json;
 
         }
 
@@ -26,5 +27,27 @@ namespace JsonDictionary
 
 
         //Skriv Json fil
+        public void WriteJsonFile(string json, string fileName)
+        {
+            if (IsValidJson(json))
+            {
+                string filePath = Path.Combine(Environment.CurrentDirectory, fileName);
+                File.WriteAllText(filePath, json);
+            }
+
+        }
+
+
+        /// <summary>
+        /// Validates a string as JSON. If invalid JSON, this method will throw a JsonReaderException.
+        /// </summary>
+        /// <param name="stringToValidate"></param>
+        /// <returns>Returns true if input is correct JSON format, otherwise throws JsonReaderException.</returns>
+        /// <exception cref="JsonReaderException"></exception>
+        private bool IsValidJson(string stringToValidate)
+        {
+            JToken.Parse(stringToValidate);
+            return true;
+        }
     }
 }
